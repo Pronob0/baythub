b2d5e275b6f6b1a3671aa95501cc44f55530c9a9
pronob <pronobsarker16@gmail.com>
2024-07-09 15:21:21 +0600

    invest

--- project/resources/views/frontend/invest/invest-details.blade.php
+++ project/resources/views/frontend/invest/invest-details.blade.php
@@ -245,6 +245,272 @@
                         </div>
     
                     </div>
+
+
+
+                    <div class="block-wrap">
+    
+                        <div class="block-header">
+                            <h4 class="block-title">@lang('Units Available')</h4>
+                        </div>
+    
+                        <div class="block-body">
+                            @php
+                                $bedsize = json_decode($item->bed_sizes);
+                                $bedprice = json_decode($item->bed_prices);
+                            @endphp
+
+                            
+                            <div class="accordion" id="accordionExample">
+                                @if(isset($bedsize->onebed))
+
+                                <div class="accordion-item">
+                                  <h2 class="accordion-header" id="headingOne">
+                                    <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                      1 Bed
+                                    </button>
+                                  </h2>
+                                  <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                                    <div class="accordion-body">
+
+                                        <table class="table table-light">
+                                            <thead>
+                                                <tr>
+                                                  <th scope="col">#</th>
+                                                  <th scope="col">Size</th>
+                                                  <th scope="col">Price</th>
+                                                </tr>
+                                              </thead>
+                                            <tbody>
+                                                @foreach (array_combine($bedsize->onebed,$bedprice->onebed) as $size=>$price)
+                                                <tr>
+                                                    <td>Type {{ $loop->iteration }}</td>
+                                                    <td>{{ $size }} Sqft</td>
+                                                    <td>{{ $price }} {{ $currency->name }}</td>
+                                                    
+                                                </tr>
+                                                @endforeach
+                                            </tbody>
+                                        </table>
+                                        
+                                        
+                                    </div>
+                                  </div>
+                                </div>
+                                @endif
+
+                                @if(isset($bedsize->twobed))
+                                <div class="accordion-item">
+                                  <h2 class="accordion-header" id="headingTwo">
+                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                                      2 Beds
+                                    </button>
+                                  </h2>
+                                  <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                                    <div class="accordion-body">
+
+                                        <table class="table table-light">
+                                            <thead>
+                                                <tr>
+                                                  <th scope="col">#</th>
+                                                  <th scope="col">Size</th>
+                                                  <th scope="col">Price</th>
+                                                </tr>
+                                              </thead>
+                                            <tbody>
+                                                @foreach (array_combine($bedsize->twobed,$bedprice->twobed) as $size=>$price)
+                                                <tr>
+                                                    <td>Type {{ $loop->iteration }}</td>
+                                                    <td>{{ $size }} Sqft</td>
+                                                    <td>{{ $price }} {{ $currency->name }}</td>
+                                                </tr>
+                                                @endforeach
+                                            </tbody>
+                                        </table>
+                                      
+                                    </div>
+                                  </div>
+                                </div>
+                                @endif
+
+                                @if(isset($bedsize->threebed))
+                                <div class="accordion-item">
+                                  <h2 class="accordion-header" id="headingThree">
+                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                                      3 Beds
+                                    </button>
+                                  </h2>
+                                  <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+                                    <div class="accordion-body">
+
+                                        <table class="table table-light">
+                                            <thead>
+                                                <tr>
+                                                  <th scope="col">#</th>
+                                                  <th scope="col">Size</th>
+                                                  <th scope="col">Price</th>
+                                                </tr>
+                                              </thead>
+                                            <tbody>
+                                                @foreach (array_combine($bedsize->threebed,$bedprice->threebed) as $size=>$price)
+                                                <tr>
+                                                    <td>Type {{ $loop->iteration }}</td>
+                                                    <td>{{ $size }} Sqft</td>
+                                                    <td>{{ $price }} {{ $currency->name }}</td>
+                                                </tr>
+                                                @endforeach
+                                            </tbody>
+                                        </table>
+                                      
+                                    </div>
+                                  </div>
+                                </div>
+                                @endif
+
+                                @if(isset($bedsize->fourbed))
+                                <div class="accordion-item">
+                                  <h2 class="accordion-header" id="headingFour">
+                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+                                      4 Beds
+                                    </button>
+                                  </h2>
+                                  <div id="collapseFour" class="accordion-collapse collapse" aria-labelledby="headingFour" data-bs-parent="#accordionExample">
+                                    <div class="accordion-body">
+
+                                        <table class="table table-light">
+                                            <thead>
+                                                <tr>
+                                                  <th scope="col">#</th>
+                                                  <th scope="col">Size</th>
+                                                  <th scope="col">Price</th>
+                                                </tr>
+                                              </thead>
+                                            <tbody>
+                                                @foreach (array_combine($bedsize->fourbed,$bedprice->fourbed) as $size=>$price)
+                                                <tr>
+                                                    <td>Type {{ $loop->iteration }}</td>
+                                                    <td>{{ $size }} Sqft</td>
+                                                    <td>{{ $price }} {{ $currency->name }}</td>
+                                                </tr>
+                                                @endforeach
+                                            </tbody>
+                                        </table>
+                                      
+                                    </div>
+                                  </div>
+                                </div>
+                                @endif
+
+                                @if(isset($bedsize->fivebed))
+                                <div class="accordion-item">
+                                  <h2 class="accordion-header" id="headingFive">
+                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
+                                      5 Beds
+                                    </button>
+                                  </h2>
+                                  <div id="collapseFive" class="accordion-collapse collapse" aria-labelledby="headingFive" data-bs-parent="#accordionExample">
+                                    <div class="accordion-body">
+
+                                        <table class="table table-light">
+                                            <thead>
+                                                <tr>
+                                                  <th scope="col">#</th>
+                                                  <th scope="col">Size</th>
+                                                  <th scope="col">Price</th>
+                                                </tr>
+                                              </thead>
+                                            <tbody>
+                                                @foreach (array_combine($bedsize->fivebed,$bedprice->fivebed) as $size=>$price)
+                                                <tr>
+                                                    <td>Type {{ $loop->iteration }}</td>
+                                                    <td>{{ $size }} Sqft</td>
+                                                    <td>{{ $price }} {{ $currency->name }}</td>
+                                                </tr>
+                                                @endforeach
+                                            </tbody>
+                                        </table>
+                                      
+                                    </div>
+                                  </div>
+                                </div>
+                                @endif
+
+                                @if(isset($bedsize->sixbed))
+                                <div class="accordion-item">
+                                  <h2 class="accordion-header" id="headingSix">
+                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseSix" aria-expanded="false" aria-controls="collapseSix">
+                                      6 Beds
+                                    </button>
+                                  </h2>
+                                  <div id="collapseSix" class="accordion-collapse collapse" aria-labelledby="headingSix" data-bs-parent="#accordionExample">
+                                    <div class="accordion-body">
+
+                                        <table class="table table-light">
+                                            <thead>
+                                                <tr>
+                                                  <th scope="col">#</th>
+                                                  <th scope="col">Size</th>
+                                                  <th scope="col">Price</th>
+                                                </tr>
+                                              </thead>
+                                            <tbody>
+                                                @foreach (array_combine($bedsize->sixbed,$bedprice->sixbed) as $size=>$price)
+                                                <tr>
+                                                    <td>Type {{ $loop->iteration }}</td>
+                                                    <td>{{ $size }} Sqft</td>
+                                                    <td>{{ $price }} {{ $currency->name }}</td>
+                                                </tr>
+                                                @endforeach
+                                            </tbody>
+                                        </table>
+                                      
+                                    </div>
+                                  </div>
+                                </div>
+                                @endif
+
+
+                                @if(isset($bedsize->sevenbed))
+                                <div class="accordion-item">
+                                  <h2 class="accordion-header" id="headingSeven">
+                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseSeven" aria-expanded="false" aria-controls="collapseSeven">
+                                      7 Beds
+                                    </button>
+                                  </h2>
+                                  <div id="collapseSeven" class="accordion-collapse collapse" aria-labelledby="headingSeven" data-bs-parent="#accordionExample">
+                                    <div class="accordion-body">
+
+                                        <table class="table table-light">
+                                            <thead>
+                                                <tr>
+                                                  <th scope="col">#</th>
+                                                  <th scope="col">Size</th>
+                                                  <th scope="col">Price</th>
+                                                </tr>
+                                              </thead>
+                                            <tbody>
+                                                @foreach (array_combine($bedsize->sevenbed,$bedprice->sevenbed) as $size=>$price)
+                                                <tr>
+                                                    <td>Type {{ $loop->iteration }}</td>
+                                                    <td>{{ $size }} Sqft</td>
+                                                    <td>{{ $price }} {{ $currency->name }}</td>
+                                                </tr>
+                                                @endforeach
+                                            </tbody>
+                                        </table>
+                                      
+                                    </div>
+                                  </div>
+                                </div>
+                                @endif
+
+
+                              </div>
+
+                            
+                        </div>
+    
+                    </div>
                     
     
                     <div class="block-wrap">
--- project/resources/views/user/investment/bed-size.blade.php
+++ project/resources/views/user/investment/bed-size.blade.php
@@ -11,14 +11,12 @@
     </div>
     <div id="bed1-fields">
         <div class="row bed-field" id="bed1-1">
-            @foreach ($bedsize->onebed as $size)
+            @foreach (array_combine($bedsize->onebed,$bedprice->onebed)  as $size=>$price)
             <div class="form-group col-md-5">
                 <label>1 Bed Size</label>
                 <input type="number" class="form-control" value="{{ $size }}" name="1bed_size[]" placeholder="Bed Size in sqft">
             </div>
-            @endforeach
-            
-            @foreach ($bedprice->onebed as $price)
+
             <div class="form-group col-md-5">
                 <label>1 Bed Price</label>
                 <input type="number" class="form-control" value="{{ $price }}" name="1bed_price[]" placeholder="Bed Price">
@@ -71,19 +69,19 @@
     </div>
     <div id="bed2-fields">
         <div class="row bed-field" id="bed2-1">
-            @foreach ($bedsize->twobed as $size)
+            @foreach (array_combine($bedsize->twobed, $bedprice->twobed) as $size=>$price)
             <div class="form-group col-md-5">
                 <label>2 Bed Size</label>
                 <input type="number" class="form-control" value="{{ $size }}" name="2bed_size[]" placeholder="Bed Size in sqft">
             </div>
-            @endforeach
 
-            @foreach ($bedprice->twobed as $price)
             <div class="form-group col-md-5">
                 <label>2 Bed Price</label>
                 <input type="number" class="form-control" value="{{ $price }}" name="2bed_price[]" placeholder="Bed Price">
             </div>
             @endforeach
+
+            
             @if (isset($bedsize->twobed[0]))
                 <div class="col-md-2">
                     <a href="javascript:;" class="btn btn-sm btn-primary mt-5 add-more" data-bed="2">Add more</a>
@@ -132,19 +130,18 @@
     </div>
     <div id="bed2-fields">
         <div class="row bed-field" id="bed3-1">
-            @foreach ($bedsize->threebed as $size)
+            @foreach (array_combine($bedsize->threebed, $bedprice->threebed) as $size=>$price)
             <div class="form-group col-md-5">
                 <label>3 Bed Size</label>
                 <input type="number" class="form-control" value="{{ $size }}" name="3bed_size[]" placeholder="Bed Size in sqft">
             </div>
-            @endforeach
 
-            @foreach ($bedprice->threebed as $price)
             <div class="form-group col-md-5">
                 <label>3 Bed Price</label>
                 <input type="number" class="form-control" value="{{ $price }}" name="3bed_price[]" placeholder="Bed Price">
             </div>
             @endforeach
+
             @if (isset($bedsize->threebed[0]))
                 <div class="col-md-2">
                     <a href="javascript:;" class="btn btn-sm btn-primary mt-5 add-more" data-bed="3">Add more</a>
@@ -191,19 +188,18 @@
     </div>
     <div id="bed2-fields">
         <div class="row bed-field" id="bed4-1">
-            @foreach ($bedsize->fourbed as $size)
+            @foreach (array_combine($bedsize->fourbed, $bedprice->fourbed) as $size=>$price)
             <div class="form-group col-md-5">
                 <label>4 Bed Size</label>
                 <input type="number" class="form-control" value="{{ $size }}" name="4bed_size[]" placeholder="Bed Size in sqft">
             </div>
-            @endforeach
 
-            @foreach ($bedprice->fourbed as $price)
             <div class="form-group col-md-5">
                 <label>4 Bed Price</label>
                 <input type="number" class="form-control" value="{{ $price }}" name="4bed_price[]" placeholder="Bed Price">
             </div>
             @endforeach
+
             @if (isset($bedsize->fourbed[0]))
                 <div class="col-md-2">
                     <a href="javascript:;" class="btn btn-sm btn-primary mt-5 add-more" data-bed="4">Add more</a>
@@ -249,19 +245,18 @@
     </div>
     <div id="bed2-fields">
         <div class="row bed-field" id="bed5-1">
-            @foreach ($bedsize->fivebed as $size)
+            @foreach (array_combine($bedsize->fivebed, $bedprice->fivebed) as $size=>$price)
             <div class="form-group col-md-5">
                 <label>5 Bed Size</label>
                 <input type="number" class="form-control" value="{{ $size }}" name="5bed_size[]" placeholder="Bed Size in sqft">
             </div>
-            @endforeach
 
-            @foreach ($bedprice->fivebed as $price)
             <div class="form-group col-md-5">
                 <label>5 Bed Price</label>
                 <input type="number" class="form-control" value="{{ $price }}" name="5bed_price[]" placeholder="Bed Price">
             </div>
             @endforeach
+
             @if (isset($bedsize->fivebed[0]))
                 <div class="col-md-2">
                     <a href="javascript:;" class="btn btn-sm btn-primary mt-5 add-more" data-bed="5">Add more</a>
@@ -307,19 +302,18 @@
     </div>
     <div id="bed2-fields">
         <div class="row bed-field" id="bed6-1">
-            @foreach ($bedsize->sixbed as $size)
+            @foreach (array_combine($bedsize->sixbed, $bedprice->sixbed) as $size=>$price)
             <div class="form-group col-md-5">
                 <label>6 Bed Size</label>
                 <input type="number" class="form-control" value="{{ $size }}" name="6bed_size[]" placeholder="Bed Size in sqft">
             </div>
-            @endforeach
 
-            @foreach ($bedprice->sixbed as $price)
             <div class="form-group col-md-5">
                 <label>6 Bed Price</label>
                 <input type="number" class="form-control" value="{{ $price }}" name="6bed_price[]" placeholder="Bed Price">
             </div>
             @endforeach
+
             @if (isset($bedsize->sixbed[0]))
                 <div class="col-md-2">
                     <a href="javascript:;" class="btn btn-sm btn-primary mt-5 add-more" data-bed="6">Add more</a>
@@ -365,19 +359,18 @@
     </div>
     <div id="bed2-fields">
         <div class="row bed-field" id="bed7-1">
-            @foreach ($bedsize->sevenbed as $size)
+            @foreach (array_combine($bedsize->sevenbed,$bedprice->sevenbed) as $size=>$price)
             <div class="form-group col-md-5">
                 <label>7 Bed Size</label>
                 <input type="number" class="form-control" value="{{ $size }}" name="7bed_size[]" placeholder="Bed Size in sqft">
             </div>
-            @endforeach
 
-            @foreach ($bedprice->sevenbed as $price)
             <div class="form-group col-md-5">
                 <label>7 Bed Price</label>
                 <input type="number" class="form-control" value="{{ $price }}" name="7bed_price[]" placeholder="Bed Price">
             </div>
             @endforeach
+
             @if (isset($bedsize->sevenbed[0]))
                 <div class="col-md-2">
                     <a href="javascript:;" class="btn btn-sm btn-primary mt-5 add-more" data-bed="7">Add more</a>
